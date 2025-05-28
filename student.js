5b8613c9077c750c95925139816c19a77f26b304
yaswanthgitcode <yaswanthgiet@gmail.com>
2025-05-28 20:42:43 +0530

    new

--- student.js
+++ student.js
@@ -23,9 +23,7 @@ import {
     collection,
     getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import {
-    onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 
 onAuthStateChanged(auth, async (user) => {
     if (user) {
