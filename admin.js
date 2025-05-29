79cd7e9a1d8fccb7ef705a97a4330baf2e6fa99a
yaswanthgitcode <yaswanthgiet@gmail.com>
2025-05-29 20:09:43 +0530

    v

--- admin.js
+++ admin.js
@@ -7,13 +7,15 @@ import {
   doc
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAts3B-PLGCIAd6F5SdLAEDJ92PgUMZVZI",
   authDomain: "collegewebsite-75f2e.firebaseapp.com",
   projectId: "collegewebsite-75f2e",
-  storageBucket: "collegewebsite-75f2e.appspot.com",
+  storageBucket: "collegewebsite-75f2e.firebasestorage.app",
   messagingSenderId: "1070493768081",
   appId: "1:1070493768081:web:627a95539805e9e7bf2230",
+  measurementId: "G-78LC2BG67K"
 };
 
 
